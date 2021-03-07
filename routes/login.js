982e5d1b918a8b6fb9cfca431646f4be72d547b8
Caitie Garrood <caitlinanneg@gmail.com>
2021-03-07 15:40:58 -0500

    Adds routes

---
+++ routes/login.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  //route to send login information
+  router.post("/", (req, res) => {
+
+  });
+  return router;
+};
---
+++ routes/logout.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  //route to post logout
+  router.post("/", (req, res) => {
+  
+  });
+  return router;
+};
