e4fd3257d0b9051af13eae450a169e5f436cba6a
imansawi <imansawi@hotmail.com>
2021-03-08 19:59:50 +0000

    Create user_role to grant privilage to the user labber

---
+++ db/01_user_roles/table_roles.sql
@@ -0,0 +1,6 @@
+grant all privileges on table users to labber;
+grant all privileges on table stories to labber;
+grant all privileges on table user_contributions to labber;
+grant all privileges on table story_themes to labber;
+grant all privileges on table upvotes to labber;
+grant all privileges on table favourite_stories to labber;
