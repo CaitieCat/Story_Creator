2a0246c411677eb63d13b2614828a70fc79f59bf
imansawi <imansawi@hotmail.com>
2021-03-09 17:42:17 +0000

    Add queries to databes for routes

--- db/01_queries/02_stories.sql
+++ db/01_queries/02_stories.sql
@@ -7,8 +7,8 @@ JOIN themes ON themes(id) = theme_id
 -- JOIN user_contributions ON stories(id) = id;
 
 SELECT title, content, theme_id, dtory, image, status 
-FROM  stories
-JOIN favourite-stories ON favourite-stories(id) = id;
+FROM stories
+LEFT JOIN favourite_stories ON favourite_stories(story_id) = id;
 
 INSERT INTO stories (user_id, title, content, created_at, story_image, theme_id)
 VALUES (2, 'title1', 'contentcontentstory1', NOW(), '1254', 1);
