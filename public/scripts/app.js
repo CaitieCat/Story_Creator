b6ed87fb1d9875fef14e36e1eb849f9ff1de1da4
imansawi <imansawi@hotmail.com>
2021-03-11 03:36:00 +0000

    Add functionalities to stories page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,20 @@
+const stories = require("../../routes/stories");
 
 localStorage;// TODO: Decide what to do about this .ajax request for users /api/users
 // const { newStory } = require('./public/scripts/helpers');
 
 $(() => {
     console.log("test")
-    const displayStories = function(){
-      $.ajax({
-      method: "GET",
-      url: "http://localhost:8080/stories"
-      }).done((stories) => {
-        $(".stories-container").empty();
-        loadStories(stories)
-      });
-    };
 
-  displayStories();
-
-  const loadStories = function(stories) {
-    for (const each of stories){
+  const loadStories = function(listOfStories) {
+    for (const each of listOfStories){
       const storyResults = newStory(each);
-      $(".story").prepend(storyResults);
+      $(".incomplete-stories").prepend(storyResults);
     }
   }
 
   const newStory = function(story) {
-    const $story = `
+    const story = `
     <div class="story">
           <header>
             <a href="/stories/${story.id}">${story.title}</a>
@@ -38,6 +28,24 @@ $(() => {
           </footer>
         </div>
   `
-   return $story;
+   return story;
   }
+
+  const displayStories = function(){
+    $.ajax({
+    method: "GET",
+    url: "http://localhost:8080/stories"
+    })
+    .done((stories) => {
+      $(".stories-container").empty();
+      console.log(stories);
+      loadStories(stories);
+    })
+    .fail(function () {
+      alert("Story not found");
+    });
+  };
+
+displayStories();
+
 });
--- routes/stories.js
+++ routes/stories.js
@@ -3,69 +3,35 @@
 
 const express = require('express');
 const router  = express.Router();
+const {allStories} = require('../lib/dbhelpers/stories_db_helper');
 //const { newStory } = require('./public/scripts/helpers');
 
-module.exports = (db) => {
+module.exports = (db) => { 
+  // can be used if there is an Admin page to list all the clients /* router.get 
   //render the stories page to view all stories
   router.get("/", (req, res) => {
-    res.render("stories");
+    console.log("Redirected to stories");
+    allStories(db)
+    .then((stories) => {
+      const completeStories = [];
+      const inprogressStories = [];
+      const tempVar = {completeStories, inprogressStories};
+      console.log(stories);
+      for(let story of stories) {
+        if (!story.story_status) {
+          inprogressStories.push(story);
+        } else {
+          completeStories.push(story);
+        } 
+      }
+    res.render("stories", tempVar);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+   
   });
     
-  //post a new story
-  // router.post("/", (req, res) => {
-  //   //const user_id = req.cookies['user_id'];
-  //   const user_name = req.cookies['user_name'];
-  //   //const title = req.body.title;
-  //   const content = req.body.content;
-  //   const created_at = NOW();
-  //   const story_image = req.body.story_image;
-  //   const theme_id = req.body.theme_id;
-  //   const values = [user_id, title, content, created_at, story_image, theme_id];
-  //   console.log("Posted a story!");
-  //   console.log("Values", values);
-  //   console.log('values =', values);
-    
-  //   db.query(
-  //     `INSERT INTO stories (user_id, title, content, created_at, story_image, theme_id)
-  //     VALUES ($1, $2, $3, $4, $5, $6)`, values)
-  //       .then((data) => {
-  //         console.log("Post New Story", res.data);
-  //         res.redirect("/stories");
-  //       })
-  //       .catch((err) => {
-  //         res.status(500).json({ error: err.message });
-  //       });
-  //   });
-
-// router.get("/:story_id", (req, res) => {
-//   res.render("user_contributions");
-// });
-
-//post a new story
-// router.post("/", (req, res) => {
-//   const story_id = req.body.story_id;
-//   const user_id = req.body.user_id;
-//   const contribution = req.body.contribution;
-//   const contributed_at = NOW();
-//   const accepted = req.body.accepted;
-//   const values = [story_id, user_id, contribution, contributed_at, accepted];
-  
-//   console.log("Posted a story!");
-//   console.log("Values", values);
-  
-//   db.query(
-//     `INSERT INTO user_contributions (story_id, user_id, contribution, contributed_at, accepted)
-//     VALUES ($1, $2, $3, $4, $5)`, values)
-//       .then((data) => {
-//         console.log("Post New Story", res.data);
-//         res.redirect("/stories");
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-// return router;
-// };
   router.post('/', (req, res) =>{
     console.log("I'm a post!");
     const user_id = 1;
@@ -77,10 +43,15 @@ module.exports = (db) => {
     console.log(values);
     db.query(
         `INSERT INTO stories (user_id, title, content, created_at, theme_id)
-        VALUES ($1, $2, $3, $4, $5)`, values)
+        VALUES ($1, $2, $3, $4, $5) RETURNING *`, values)
           .then((data) => {
             console.log("Posted New Story", res.data);
-            res.redirect("/stories");
+            //append new story
+      
+            for (const each of res.data){
+
+            }
+            res.render("/stories", stories);
           })
           .catch((err) => {
             res.status(500).json({ error: err.message });
